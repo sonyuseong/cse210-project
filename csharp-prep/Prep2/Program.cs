9260afe1807776702764e3d2e83855815644d33d
sonyuseong <Son21009@byui.edu>
2025-04-26 17:18:16 -0600

    w01 prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?");
+        string answer = Console.ReadLine();
+        int gradePercent = int.Parse(answer);
+        string letter = "";
+
+        if (gradePercent >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is: {letter}");
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("Congratulate! You passed the class");
+        }
+        else
+        {
+            Console.WriteLine("Seriously? You're out");
+        }
     }
 }
