10c0ecbb39df7bc67001cea25c81d06dfd83082e
sonyuseong <Son21009@byui.edu>
2025-04-28 10:20:27 -0600

    w02 prep3,4,5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,100);
+        bool isContinue = true;
+        do{
+            Console.Write("What is your guess? ");
+            string answer = Console.ReadLine();
+            int number = int.Parse(answer);
+
+            if (number == magicNumber){
+                Console.WriteLine("You guessed it!");
+                isContinue = false;
+            }else if (number > magicNumber){
+                Console.WriteLine("Lower");
+            }else{
+                Console.WriteLine("Higher");
+            }
+        }while (isContinue);
+        
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number = 1;
+
+        while (number != 0){
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number > 0){
+                numbers.Add(number);
+            }
+        }
+
+        int sum = 0;
+        foreach(int num in numbers){
+            sum = sum + num;
+        }
+        Console.WriteLine($"The Sum is: {sum}");
+
+        float average = 0;
+        average = sum/numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int largestNumber = 0;
+        foreach(int num in numbers){
+            if (largestNumber < num){
+                largestNumber = num;
+            }
+        }
+        Console.WriteLine($"The largest number is: {largestNumber}");
     }
 }
