af20747123861d8c573a30af6cfa8464da058611
sonyuseong <Son21009@byui.edu>
2025-05-05 09:57:14 -0600

    w03 prepare

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,9 @@
+public class Job {
+    public string _company;
+    public string _jobTitle;
+    public int _startYear, _endYear;
+
+    public void Display() {
+        Console.WriteLine($"{_jobTitle} {_company} ({_startYear} - {_endYear})");
+    }
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Janitor";
+        job1._company = "BYU-Idaho";
+        job1._startYear = 2021;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Samsung";
+        job2._startYear = 2024;
+        job2._endYear = 2026;
+
+
+        job1.Display();
+
+        Resume resume = new Resume();
+        resume._name = "Yuseong Son";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
+    
