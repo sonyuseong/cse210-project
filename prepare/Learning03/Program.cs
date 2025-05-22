11ababd6a380691ca767cf874241cd459a1ac336
sonyuseong <Son21009@byui.edu>
2025-05-21 18:54:10 -0600

    w05 activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        
+        Fraction case1 = new Fraction();
+        Console.WriteLine(case1.GetFractionString());
+        Console.WriteLine(case1.GetDecimalValue());
+
+        Fraction case2 = new Fraction(5);
+        Console.WriteLine(case2.GetFractionString());
+        Console.WriteLine(case2.GetDecimalValue());
+
+        Fraction case3 = new Fraction(3,4);
+        Console.WriteLine(case3.GetFractionString());
+        Console.WriteLine(case3.GetDecimalValue());
+
+        Fraction case4 = new Fraction(1,3);
+        Console.WriteLine(case4.GetFractionString());
+        Console.WriteLine(case4.GetDecimalValue());
     }
 }
